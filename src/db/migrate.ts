a39c39a310bcacf1966796fc0ce2cb3afa1b3ead
unknown <jeivi.777@gmail.com>
2024-01-18 08:53:55 -0300

    .

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -4,7 +4,7 @@ import chalk from 'chalk'
 import { drizzle } from 'drizzle-orm/postgres-js'
 import { migrate } from 'drizzle-orm/postgres-js/migrator'
 
-import { env } from '@/env'
+import { env } from '../env'
 
 const connection = postgres(env.DB_URL, { max: 1 })
 const db = drizzle(connection)
